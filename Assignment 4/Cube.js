9b272ac67da639f0cc670f905ce6b0c40c8b47d9
RonNofar <ronnofar2@gmail.com>
2017-02-28 20:53:07 -0800

    Update Assignment 4
    For some weird reason nothing but the background color will render

--- Assignment 4/Cube.js
+++ Assignment 4/Cube.js
@@ -1,32 +1,36 @@
-function Cube() {
-	this.count = 8;
-	
-	this.positions = {
-		values : new Float32Array([
-			0.0, 0.0, 0.0 // V0
-			1.0, 0.0, 0.0 // V1
-			1.0, 1.0, 0.0 // V2
-			0.0, 1.0, 0.0 // V3
-			0.0, 1.0, 1.0 // V4
-			0.0, 0.0, 1.0 // V5
-			1.0, 1.0, 1.0 // V6
-			1.0, 0.0, 1.0 // V7
-		])
-	};
-	this.indices = {
-		values = new Uint16Array([
-			0,1,3, // Front 1
-			1,2,3, // Front 2
-			1,2,7, // Right 1
-			2,6,7, // Right 2
-			4,6,7, // Back 1
-			4,5,7, // Back 2
-			0,1,7, // Bottom 1
-			0,5,7, // Bottom 2
-			0,3,5, // Left 1
-			3,4,5, // Left 2
-			2,3,6, // Top 1
-			3,4,6  // Top 2
-		])
-	};
+
+var cube = undefined;
+var gl = undefined;
+var angle = 0;
+
+function init() {
+  var canvas = document.getElementById( "webgl-canvas" );
+
+  gl = WebGLUtils.setupWebGL( canvas );
+
+  if ( !gl ) {
+    alert("Unable to setup WebGL");
+    return;
+  }
+
+  gl.clearColor( 0.8, 0.8, 0.8, 1.0 );
+  gl.enable( gl.DEPTH_TEST );
+
+  cube = new Cube();
+
+  render();
 }
+
+function render() {
+  gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+
+  angle += 2.0; // degrees
+
+  cube.MV = rotate( angle, [1, 1, 0] );
+
+  cube.render();
+
+  requestAnimationFrame( render ); // schedule another call to render()
+}
+
+window.onload = init;
---
+++ Common/Cube.js
@@ -0,0 +1,81 @@
+
+function Cube( vertexShaderId, fragmentShaderId ) {
+
+    // Initialize the shader pipeline for this object using either shader ids
+    //   declared in the application's HTML header, or use the default names.
+    //
+    var vertShdr = vertexShaderId || "Cube-vertex-shader";
+    var fragShdr = fragmentShaderId || "Cube-fragment-shader";
+
+    this.program = initShaders(gl, vertShdr, fragShdr);
+
+    if ( this.program < 0 ) {
+        alert( "Error: Cube shader pipeline failed to compile.\n\n" +
+            "\tvertex shader id:  \t" + vertShdr + "\n" +
+            "\tfragment shader id:\t" + fragShdr + "\n" );
+        return; 
+    }
+
+    this.positions = { 
+        values : new Float32Array([
+            0.0, 0.0, 0.0 // V0
+			1.0, 0.0, 0.0 // V1
+			0.0, 1.0, 0.0 // V2
+			1.0, 1.0, 0.0 // V3
+			0.0, 0.0, 1.0 // V4
+			1.0, 0.0, 1.0 // V5
+			0.0, 1.0, 1.0 // V6
+			1.0, 1.0, 1.0 // V7
+            ]),
+        numComponents : 8
+    };
+    
+    this.indices = { 
+        values : new Uint16Array([
+            0,1,3, // Front 1
+			0,3,2, // Front 2
+			1,5,7, // Right 1
+			1,7,3, // Right 2
+			6,7,5, // Back 1
+			6,5,4, // Back 2
+			4,1,0, // Bottom 1
+			4,5,1, // Bottom 2
+			4,0,2, // Left 1
+			4,2,6, // Left 2
+			2,3,7, // Top 1
+			2,7,6  // Top 2
+        ])
+    };
+    this.indices.count = this.indices.values.length;
+
+    
+    this.positions.buffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
+    gl.bufferData( gl.ARRAY_BUFFER, this.positions.values, gl.STATIC_DRAW );
+
+    this.indices.buffer = gl.createBuffer();
+    gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
+    gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, this.indices.values, gl.STATIC_DRAW );
+
+    this.positions.attributeLoc = gl.getAttribLocation( this.program, "vPosition" );
+    gl.enableVertexAttribArray( this.positions.attributeLoc );
+
+    MVLoc = gl.getUniformLocation( this.program, "MV" );
+
+    this.MV = undefined;
+
+    this.render = function () {
+        gl.useProgram( this.program );
+
+        gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
+        gl.vertexAttribPointer( this.positions.attributeLoc, this.positions.numComponents,
+            gl.FLOAT, gl.FALSE, 0, 0 );
+ 
+        gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
+
+        gl.uniformMatrix4fv( MVLoc, gl.FALSE, flatten(this.MV) );
+
+        // Draw the cube's base
+        gl.drawElements( gl.TRIANGLES, this.indices.count, gl.UNSIGNED_SHORT, 0 );
+    }
+};
